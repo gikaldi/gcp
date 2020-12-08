63fa17640f1224eea1ff6e93c913a39c7f2ecb13
Gikaldi Arbiyan <gikaldiarbiyans18@if.unjani.ac.id>
2020-12-08 14:18:05 +0700

    tambah hello.java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello
+{
+	/**
+	 *Program pertama saya Gikaldi
+	 */
+	 public static void main (String[] args) {
+		//menampilkan kata "Welcome to java programming" dilayar
+		System.out.println("Welcome to java programming!");
+	}
+}
